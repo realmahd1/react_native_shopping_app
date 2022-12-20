5e2b05e1eba891b5904279228e9cdcd4b7b7fa96
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-12-20 18:38:50 +0330

    feat(productInfo) : get product info by productId

--- app/screens/ProductInfo.js
+++ app/screens/ProductInfo.js
@@ -1,12 +1,30 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Items } from '../components/Database';
 
-const ProductInfo = () => {
-  return (
-    <View>
-      <Text>ProductInfo</Text>
-    </View>
-  )
+const ProductInfo = ({ route, navigation }) => {
+    const { productId } = route.params;
+    const [product, setProduct] = useState([]);
+    
+    useEffect(() => {
+        const unsubscribe = navigation.addListener("focus", () => {
+            getDataFromDB()
+        })
+        return unsubscribe
+    }, [navigation])
+    const getDataFromDB = () => {
+        for (let index = 0; index < Items.length; index++) {
+            if (Items[index].id == productId) {
+                return setProduct(Items[index]);
+            }
+        }
+    }
+
+    return (
+        <View>
+            <Text>ProductInfo{productId}</Text>
+        </View>
+    )
 }
 
 export default ProductInfo
